daea086ed6c2e5f3964847eaea1a7ef5c871f7b4
jungwoogyun <jwg8910@naver.com>
2025-08-08 17:27:39 +0900

    01 DAY

--- JAVA01/src/Ch02/TESTABCD.java
+++ JAVA01/src/Ch02/TESTABCD.java
@@ -1,5 +1,12 @@
 package Ch02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TESTABCD {
 
+	public static void main(String[] args) {
+		
+		List<String> a = new ArrayList();	
+	}
 }
