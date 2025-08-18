1e468f542e5135a0b0463c57ba57f1979437a43c
jungwoogyun <jwg8910@naver.com>
2025-08-18 11:45:10 +0900

    06 DAY

--- JAVA02/src/Ch03/C01ArrayMain.java
+++ JAVA02/src/Ch03/C01ArrayMain.java
@@ -10,6 +10,7 @@ public class C01ArrayMain {
 		
 //		int [] arr1 = new int[5];
 		int  arr1 []= new int[5];
+		
 		System.out.println("배열길이 : " + arr1.length);
 		System.out.println("5개의 값을 입력하세요");
 		arr1[0] = sc.nextInt();
