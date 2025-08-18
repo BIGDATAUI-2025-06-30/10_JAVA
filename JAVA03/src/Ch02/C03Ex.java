4ab602e457cd781cce005533e56871f02221fad6
jungwoogyun <jwg8910@naver.com>
2025-08-18 15:32:28 +0900

    06 DAY

--- JAVA03/src/Ch02/C03Ex.java
+++ JAVA03/src/Ch02/C03Ex.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 public class C03Ex {
 
-	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("나누셈 위한 두값을 입력하세요 : ");
-		int n1 = sc.nextInt();
-		int n2 = sc.nextInt();
-		System.out.println("나눗셈 결과 : " + n1/n2);	// N/0 시 예외발생!
+		try {
+			System.out.print("나누셈 위한 두값을 입력하세요 : ");
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			System.out.println("나눗셈 결과 : " + n1/n2);	// N/0 시 예외발생!
+		}catch(ArithmeticException e) {
+			System.out.println("예외처리!");
+		}
 		
 		System.out.println("실행되어야 할 코드 1");
 		System.out.println("실행되어야 할 코드 2");
