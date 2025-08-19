af6928d056e5a331479625dcd63b9d1ce4408a39
jungwoogyun <jwg8910@naver.com>
2025-08-19 12:46:28 +0900

    07 DAY

--- JAVA03/src/Ch08/ex/문제2.java
+++ JAVA03/src/Ch08/ex/문제2.java
@@ -1,6 +1,7 @@
 package Ch08.ex;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,35 +10,48 @@ public class 문제2 {
 	// DB CONN DATA
 	private static String id = "root";
 	private static String pw = "1234";
-	private static String url = "jdbc:mysql://localhost:3306/tmpdb";
+	private static String url = "jdbc:mysql://localhost:3306/opendatadb";
 
 	// JDBC참조변수
 	private static Connection conn = null; // DBMS 의 특정 DB와 연결되는 객체
 	private static PreparedStatement pstmt = null; // SQL Query 전송용 객체
 	private static ResultSet rs = null; // Select 결과물 담을 객체
 
-	public static void conn() throws SQLException {
+	public static void conn() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver Loading Success...");
+		conn = DriverManager.getConnection(url,id,pw);
+		System.out.println("DB CONNECTED...");
 	}
 
-	public static List<BookDto> selectAll() throws SQLException {
-		return null;
-	}
-
-	public static BookDto select(Long bookCode) throws SQLException {
-		return null;
-	}
+//	public static List<BookDto> selectAll() throws SQLException {
+//		return null;
+//	}
+//
+//	public static BookDto select(Long bookCode) throws SQLException {
+//		return null;
+//	}
 
 	public static int insertBook(BookDto bookDto) throws SQLException {
-		return -1;
+		
+		pstmt = conn.prepareStatement("insert into tbl_book values(?,?,?,?)");
+		pstmt.setLong(1, bookDto.getBookCode());
+		pstmt.setString(2, bookDto.getBookName());
+		pstmt.setString(3, bookDto.getPublisher());
+		pstmt.setString(4, bookDto.getIsbn());
+		//
+		int result = pstmt.executeUpdate();
+		
+		return result;
 	}
 
-	public static int updateBook(BookDto bookDto) throws SQLException {
-		return -1;
-	}
-
-	public static int deleteBook(BookDto bookDto) throws SQLException {
-		return -1;
-	}
+//	public static int updateBook(BookDto bookDto) throws SQLException {
+//		return -1;
+//	}
+//
+//	public static int deleteBook(BookDto bookDto) throws SQLException {
+//		return -1;
+//	}
 
 	public static void main(String[] args) {
 		try {
@@ -51,26 +65,29 @@ public class 문제2 {
 			insertBook(new BookDto(2L, "도서명1", "출판사명1", "isbn-1"));
 			insertBook(new BookDto(3L, "도서명1", "출판사명1", "isbn-1"));
 			insertBook(new BookDto(4L, "도서명1", "출판사명1", "isbn-1"));
-			// SELECTALL
-			Liste<BookDto> allBook = selectAll();
-			System.out.println("SelectAll : ");
-			allBook.forEach(System.out::println);
-			// SELECT
-			BookDto dto = select(1L);
-			System.out.println("select : " + dto);
-			// UPDATE
-			dto.setBookName("수정도서명-2");
-			dto.setPublisher("수정출판사명-2");
-			int r1 = updateBook(dto);
-			if (r1 > 0)
-				System.out.println("수정완료 : " + r1);
-
-			// DELETE
-			int r2 = deleteBook(dto);
-			if (r2 > 0)
-				System.out.println("삭제완료 : " + r2);
-
-			//TX END
+			
+//			// SELECTALL
+//			List<BookDto> allBook = selectAll();
+//			System.out.println("SelectAll : ");
+//			allBook.forEach(System.out::println);
+			
+			
+//			// SELECT
+//			BookDto dto = select(1L);
+//			System.out.println("select : " + dto);
+//			// UPDATE
+//			dto.setBookName("수정도서명-2");
+//			dto.setPublisher("수정출판사명-2");
+//			int r1 = updateBook(dto);
+//			if (r1 > 0)
+//				System.out.println("수정완료 : " + r1);
+//
+//			// DELETE
+//			int r2 = deleteBook(dto);
+//			if (r2 > 0)
+//				System.out.println("삭제완료 : " + r2);
+//
+//			//TX END
 		} catch (Exception e) {
 			//TX ROLLBACKALL
 		}
