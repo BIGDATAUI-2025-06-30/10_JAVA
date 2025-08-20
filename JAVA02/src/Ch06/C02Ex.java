b8bb17a4a887084052876e769dfffc70b9d33071
jungwoogyun <jwg8910@naver.com>
2025-08-20 17:38:07 +0900

    07 DAY

--- JAVA02/src/Ch06/C02Ex.java
+++ JAVA02/src/Ch06/C02Ex.java
@@ -22,46 +22,45 @@ abstract class Converter {
 
 //원화 -> 달러 
 class Won2Dollar extends Converter{
+	private double ratio;
 	
+	public Won2Dollar(double ratio) {
+		this.ratio = ratio;
+	}
 	@Override
 	protected double convert(double src) {
-		// TODO Auto-generated method stub
-		return 0;
+		return src/ratio;
 	}
-
 	@Override
 	protected String getSrcString() {
-		// TODO Auto-generated method stub
-		return null;
+		return "원화";
 	}
-
 	@Override
 	protected String getDestString() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
+		return "달러";
+	}	
 }
 
 //섭씨 -> 화씨
-//class 섭씨to화씨 extends Converter{
-//	
-//}
+class 섭씨to화씨 extends Converter{
+	
+}
 
 public class C02Ex {
 
 	public static void main(String[] args) {
 
 		//[3번] Converter 클래스를 상속받아 원화를 달러로 변환하는 Won2Dollar 클래스를 작성하라. main() 메소드와 실행 결과는 다음과 같다.
-		   Won2Dollar toDollar = new Won2Dollar(1446); // 1달러는 1446원
-		   toDollar.run();
+//		   Won2Dollar toDollar = new Won2Dollar(1399); // 1달러는 1399원
+//		   toDollar.run();
 		   
 		   //결과
 //		   원을 달러로 바꿉니다.
 //		   원을 입력하세요>> 24000
 //		   변환 결과: 20.0달러입니다
 
+		   섭씨to화씨 test = new 섭씨to화씨(); 
+		   test.run();
 
 	}
 
