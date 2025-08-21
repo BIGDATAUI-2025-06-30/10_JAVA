ef58b737e2db7628eacc72f9e936f6f3d74ceabb
ddaeng2001 <ddaeng2001@naver.com>
2025-08-21 12:23:17 +0900

    이수현MUL.ADD

--- JAVA02/src/Ch07/일조/Mul.java
+++ JAVA02/src/Ch07/일조/Mul.java
@@ -6,6 +6,16 @@ public class Mul implements Ch07.일조.Calculation{
 	public void Run(int n1, int n2) {
 		System.out.println("1조 이수현 곱셈 결과 : " + n1*n2);
 	}
+	public void Run(int n1, int n2, int n3, int n4, int n5) {
+		System.out.println("1조 이수현 곱셈 결과 : " + n1*n2*n3*n4*n5);
+	}
+	public void Run(double n1, double n2, double n3, double n4) {
+		System.out.println("1조 이수현 곱셈 결과 : " + n1 * n2* n3 * n4);
+	}
+	public void Run(String str1, String str2, String str3, String str4) {
+		System.out.println("1조 이수현 곱셈 결과 : " + str1 * str2 * str3 * str4);
+		 
+	}
 	
 
 }
