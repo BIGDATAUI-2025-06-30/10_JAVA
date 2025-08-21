6bfa4cd5077a1b63642b70ba7bf95a80a500fba9
jeonikk <meister1213@naver.com>
2025-08-21 14:07:22 +0900

    sum미완성

--- JAVA02/src/Ch07/이조/Sum.java
+++ JAVA02/src/Ch07/이조/Sum.java
@@ -8,6 +8,29 @@ public class Sum implements Ch07.이조.Calculation{
 		System.out.println("이조 전익환 덧셈결과 : "+result);
 		
 	}
+	@Override
+	public void Run(int... args) {
+		result = 0;
+		for(int num: args) {
+			result+=num;
+			System.out.println("이조 전익환 덧셈결과(n개) : "+result);
+		}
+		
+	}
+	@Override
+	public void Run(double... args) {
+		double result =0.0;
+		for(double num : args) {
+			double result1 = num;
+			System.out.println("이조 전익환 덧셈결과(n개 소수점) : "+ result1);
+		}
+		
+	}
+	@Override
+	public void Run(String... args) {
+		
+		
+	}
 
 
 }
