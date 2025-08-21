81b1a6b83efe68f47520e07ad846db391aa4ed3f
jiwon <jiwonsukjun@naver.com>
2025-08-21 12:14:51 +0900

    -

--- JAVA02/src/Ch07/사조/Sub.java
+++ JAVA02/src/Ch07/사조/Sub.java
@@ -5,7 +5,7 @@ public class Sub implements Ch07.사조.Calculation {
 	@Override
 	public void Run(int n1, int n2) {
 		System.out.println("4조정지원 뺄셈 결과 : " + (n1-n2) );
-		
+		System.out.println("dkshelsek");
 		
 	}
 
