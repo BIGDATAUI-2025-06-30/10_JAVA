20c0a540425f08621a7414f9d042edfe6a5d8966
이인호 <zergling2565@gmail.com>
2025-08-21 11:44:54 +0900

    sum의 Run() 구현 완료

--- JAVA02/src/Ch07/일조/Sum.java
+++ JAVA02/src/Ch07/일조/Sum.java
@@ -2,6 +2,14 @@ package Ch07.일조;
 
 public class Sum implements Ch07.일조.Calculation{
 
+	@Override
+	public void Run(int n1, int n2) {
+		int sum = n1 + n2;
+		System.out.printf("일조 이인호 덧셈 결과: %d", sum);
+	}
 
+	
+
+	
 
 }
