b7472495c8707ccfbe8db12b2dbaf3c438d5dc04
B735244 <dig05252@gmail.com>
2025-08-21 11:30:17 +0900

    interface upload

--- JAVA02/src/Ch07/오조/Div.java
+++ JAVA02/src/Ch07/오조/Div.java
@@ -1,5 +1,7 @@
 package Ch07.오조;
 
-public class Div {
-
+public class Div implements Calculation {
+	//함수 오버로딩으로
+		//public void run(int n1, int n2){}, public void run(int... nums){}, public void run(double... nums){}, public void run(String... strings){} 이렇게 4개를 만들어주세요.
+		//그리고 {}안에서 Scanner를 사용하여 인자들을 조건에 맞게 받아서 연산하고 그 연산식을 System.out.println("");으로 출력하는 함수까지 만들어주세요
 }
