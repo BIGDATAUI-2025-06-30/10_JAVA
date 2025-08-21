a06d0598b67444cc306cff82ca7bf0856dabf41e
이인호 <zergling2565@gmail.com>
2025-08-21 12:11:33 +0900

    Sum 클래스 완성

--- JAVA02/src/Ch07/일조/Sum.java
+++ JAVA02/src/Ch07/일조/Sum.java
@@ -3,9 +3,27 @@ package Ch07.일조;
 public class Sum implements Ch07.일조.Calculation{
 
 	@Override
-	public void Run(int n1, int n2) {
+	public void Run(int n1, int n2) {	// n1, n2 의 두 수를 받아 두 수의 합을 console창에 출력
 		int sum = n1 + n2;
-		System.out.printf("일조 이인호 덧셈 결과: %d", sum);
+		System.out.printf("일조 이인호 덧셈 결과: %d\n", sum);
+	}
+
+	@Override
+	public void Run(int n1, int n2, int n3, int n4, int n5) {
+		int sum = n1 + n2 + n3 + n4 + n5;
+		System.out.printf("일조 이인호 덧셈 결과: %d\n", sum);
+		
+	}
+
+	@Override
+	public void Run(double n1, double n2, double n3, double n4, double n5) {
+		double sum = n1 + n2 + n3 + n4 + n5;
+		System.out.printf("일조 이인호 덧셈 결과: %f\n", sum);	
+	}
+
+	@Override
+	public void Run(String s1, String s2, String s3, String s4, String s5) {
+		System.out.println(s1 + s2 + s3 + s4 + s5);
 	}
 
 	
