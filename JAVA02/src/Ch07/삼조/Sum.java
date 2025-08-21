ccc98e1bf71e99cc421c6ca464412a55c8f36463
dmg06072 <dmg06072@naver.com>
2025-08-21 12:30:07 +0900

    jjw 12:30

--- JAVA02/src/Ch07/삼조/Sum.java
+++ JAVA02/src/Ch07/삼조/Sum.java
@@ -1,11 +1,14 @@
 package Ch07.삼조;
 
-public class Sum implements Ch07.삼조.Calculation{
+public class Sum implements Ch07.삼조.Calculation {
 
 	@Override
 	public void Run(int n1, int n2) {
 		System.out.println("삼조 장지원 덧셈결과 : n1 + n2");
-		
+
 	}
 
+	public static void main(String[] args) {
+		
+	}
 }
