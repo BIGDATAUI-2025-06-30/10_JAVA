1887260168d05c04375e8a2f32759c21ab99f8fb
HaP <imseoim1120@gmail.com>
2025-08-21 12:44:17 +0900

    Hi

--- JAVA02/src/Ch07/오조/Mul.java
+++ JAVA02/src/Ch07/오조/Mul.java
@@ -1,7 +1,50 @@
 package Ch07.오조;
 
-public class Mul implements Calculation {
-	//함수 오버로딩으로
-		//public void run(int n1, int n2){}, public void run(int... nums){}, public void run(double... nums){}, public void run(String... strings){} 이렇게 4개를 만들어주세요.
-		//그리고 {}안에서 Scanner를 사용하여 인자들을 조건에 맞게 받아서 연산하고 그 연산식을 System.out.println("");으로 출력하는 함수까지 만들어주세요
+
+
+public class Mul implements Ch07.오조.Calculation{
+
+	@Override
+	public void Run(int n1, int n2) {
+		int Val =1;
+		
+		Val = n1 * n2;
+		System.out.println("5조 지원이의 두 수 곱셈값은!?" + Val);
+	}
+
+	@Override
+	public void Run(int... nums) {
+		int Val = 1;
+		for (int i = 0; i < nums.length; i++) {
+		    Val *= nums[i];   // i번째 값 꺼내서 곱하기
+		}
+		System.out.println("5조 지원이의 배열 곱셈값은!?" + Val);
+	}
+
+	@Override
+	public void Run(double... nums1) {
+		double Val = 1;
+		for (int i = 0; i < nums1.length; i++) {
+		    Val *= nums1[i];   // i번째 값 꺼내서 곱하기
+		}
+		System.out.println("5조 지원이의 배열 곱셈값은!?" + Val);
+	
+	}
+
+	@Override
+	public void Run(String... strings) {
+		StringBuilder sv = new StringBuilder();
+		    for (String s : strings) {
+		        sv.append(s);   // 그냥 이어붙이기
+		    }
+		System.out.println("5조 지원이의 문자배열 곱셈값은!?" + sv.toString());
+	
+	}
+	
 }
+
+
+	
+
+	
+	
