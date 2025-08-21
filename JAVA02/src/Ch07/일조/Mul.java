7c081c31a3a831f15265d3c1be1b3593be594fbc
ddaeng2001 <ddaeng2001@naver.com>
2025-08-21 12:40:44 +0900

    MUL.수정

--- JAVA02/src/Ch07/일조/Mul.java
+++ JAVA02/src/Ch07/일조/Mul.java
@@ -9,13 +9,17 @@ public class Mul implements Ch07.일조.Calculation{
 	public void Run(int n1, int n2, int n3, int n4, int n5) {
 		System.out.println("1조 이수현 곱셈 결과 : " + n1*n2*n3*n4*n5);
 	}
-	public void Run(double n1, double n2, double n3, double n4) {
-		System.out.println("1조 이수현 곱셈 결과 : " + n1 * n2* n3 * n4);
+	
+	@Override
+	public void Run(double n1, double n2, double n3, double n4, double n5) {
+		System.out.println("1조 이수현 곱셈 결과 : " + n1*n2*n3*n4*n5);
 	}
-	public void Run(String str1, String str2, String str3, String str4) {
-		System.out.println("1조 이수현 곱셈 결과 : " + str1 * str2 * str3 * str4);
-		 
+	
+	@Override
+	public void Run(String s1, String s2, String s3, String s4, String s5) {
+		System.out.println("1조 이수현 곱셈 결과 : " + s1+s2+s3+s4+s5);
 	}
+
 	
 
 }
