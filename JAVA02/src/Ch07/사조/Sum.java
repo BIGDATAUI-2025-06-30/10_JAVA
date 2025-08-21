d4486f8e1c4baf279845b44cca8a621fa1037c9c
NightRwby <jemin9457@naver.com>
2025-08-21 11:44:32 +0900

    Sum add

--- JAVA02/src/Ch07/사조/Sum.java
+++ JAVA02/src/Ch07/사조/Sum.java
@@ -1,5 +1,9 @@
 package Ch07.사조;
 
-public class Sum implements Calculation{
+public class Sum implements Ch07.사조.Calculation{
 
+	@Override
+	public void Run(int n1, int n2) {
+		System.out.println("일조 누구 덧셈결과 : " + n1 + n2);
+	}
 }
