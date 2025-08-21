07d66824ac3833f017790533f5cd7a9061aa2527
yexhzinx <syjuuu@gmail.com>
2025-08-21 12:36:07 +0900

    예진_완

--- JAVA02/src/Ch07/오조/Calculation.java
+++ JAVA02/src/Ch07/오조/Calculation.java
@@ -5,10 +5,10 @@ public interface Calculation {
 	//사칙연산을 위한 추상 메서드를 만들고
 	//해당 폴더에 있는 Div,Mul,Sub,Sum을 각각 역할 나눠서 함수를 완성하세요(C99Ex참고)
 	//나머지는 자유롭게 - !
-	void run(int n1, int n2) ;
-	void run(int... nums) ;
-	void run(double ...nums1);
-	void run(String... strings) ;
+	void Run(int n1, int n2) ;
+	void Run(int... nums) ;
+	void Run(double ...nums1);
+	void Run(String... strings) ;
 
 	
 }
