529cea61be32b69ca6c2948c9e1fd8d595eccaa7
이인호 <zergling2565@gmail.com>
2025-08-21 12:35:26 +0900

    no message

--- JAVA02/src/Ch07/일조/Calculation.java
+++ JAVA02/src/Ch07/일조/Calculation.java
@@ -5,17 +5,7 @@ public interface Calculation {
 	//사칙연산을 위한 추상 메서드를 만들고
 	//해당 폴더에 있는 Div,Mul,Sub,Sum을 각각 역할 나눠서 함수를 완성하세요(C99Ex참고)
 	//나머지는 자유롭게 - !
-<<<<<<< HEAD
-	void Run(int n1, int n2);	
-<<<<<<< HEAD
-	
-=======
-	void Run(int n1, int n2, int n3, int n4, int n5);
-	void Run(double n1, double n2, double n3, double n4, double n5);
-	void Run(String s1, String s2, String s3, String s4, String s5);
->>>>>>> feature/일조-이인호
-=======
-	
+
 	void Run(int n1, int n2);	
 	void Run(int n1, int n2, int n3, int n4, int n5);
 	void Run(double n1, double n2, double n3, double n4);
@@ -26,5 +16,4 @@ public interface Calculation {
 //	void Run(int n1, int n2); // n1, n2 받아 두 수의 합을 console창에 출력해주세요. 입력값은 MAX_VALUE보다 작아야합니다.
 //	int Run_by_keyboard(); // 키보드로 두 수의 입력값을 받아 
 
->>>>>>> ef58b737e2db7628eacc72f9e936f6f3d74ceabb
 }
