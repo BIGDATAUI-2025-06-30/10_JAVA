9fa0e26c182831ea9e3c2d421a596a1eca9da999
ddaeng2001 <ddaeng2001@naver.com>
2025-08-21 11:43:53 +0900

    MUL.RUN완료

--- JAVA02/src/Ch07/일조/Mul.java
+++ JAVA02/src/Ch07/일조/Mul.java
@@ -1,5 +1,11 @@
 package Ch07.일조;
 
-public class Mul {
+public class Mul implements Ch07.일조.Calculation{
+
+	@Override
+	public void Run(int n1, int n2) {
+		System.out.println("1조 이수현 곱셈 결과 : " + n1*n2);
+	}
+	
 
 }
