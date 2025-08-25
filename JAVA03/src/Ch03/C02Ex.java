2a35a1ad059e4fe9c40533d0122e0afa214c3824
jungwoogyun <jwg8910@naver.com>
2025-08-25 09:37:19 +0900

    10 DAY

---
+++ JAVA03/src/Ch03/C02Ex.java
@@ -0,0 +1,15 @@
+package Ch03;
+
+public class C02Ex {
+
+	public static void main(String[] args) {
+		
+		PopCorn<캬라멜> ob1 = new PopCorn<캬라멜>(new 캬라멜());
+		System.out.println(ob1);
+		
+		PopCorn<버터옥수수> ob2 = new PopCorn<버터옥수수>(new 버터옥수수());
+		System.out.println(ob2);
+
+	}
+
+}
