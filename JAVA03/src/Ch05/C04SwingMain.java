b1d57291a53ff9178a951345e641a5f9a0c27d0d
jungwoogyun <jwg8910@naver.com>
2025-08-26 10:12:57 +0900

    11 DAY

--- JAVA03/src/Ch05/C04SwingMain.java
+++ JAVA03/src/Ch05/C04SwingMain.java
@@ -1,10 +1,11 @@
 package Ch05;
 
-import java.awt.Color;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 class C04GUI extends JFrame
 {
@@ -14,7 +15,6 @@ class C04GUI extends JFrame
 		setBounds(100,100,500,500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
 		//Panel Setting
 		JPanel panel = new JPanel();
 		panel.setLayout(null);
@@ -22,14 +22,25 @@ class C04GUI extends JFrame
 		//Component Setting
 		JButton btn1 = new JButton("BTN01");
 		btn1.setBounds(10,10,100,30);
+		JButton btn2 = new JButton("BTN02");
+		btn2.setBounds(120,10,100,30);
+		JTextField txt1 = new JTextField();
+		txt1.setBounds(10,50,210,30);
+		JTextArea area1 = new JTextArea();
+//		area1.setBounds(10,90,210,300);
+		JScrollPane scroll1 = new JScrollPane(area1);
+		scroll1.setBounds(10,90,210,300);
 		
 		//Panel에 Component 추가
 		panel.add(btn1);
+		panel.add(btn2);
+		panel.add(txt1);
+//		panel.add(area1);
+		panel.add(scroll1);
 		
 		//Frame에 Panel 추가
 		add(panel);	 
-		
-		
+			
 		//Frame 표시 여부
 		setVisible(true);
 	}
