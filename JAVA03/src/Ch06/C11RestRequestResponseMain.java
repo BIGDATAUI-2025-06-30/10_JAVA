fd0ba1b1d850ac1cc6d53b811ce7d8a83241bb4f
jungwoogyun <jwg8910@naver.com>
2025-08-28 10:44:19 +0900

    13 DAY

--- JAVA03/src/Ch06/C11RestRequestResponseMain.java
+++ JAVA03/src/Ch06/C11RestRequestResponseMain.java
@@ -6,6 +6,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class C11RestRequestResponseMain {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
@@ -22,9 +25,25 @@ public class C11RestRequestResponseMain {
 		//03 Request 요청 -> Response 받기
 		HttpClient httpClient = HttpClient.newHttpClient();
 		HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-		System.out.println(response.body());
+//		System.out.println(response.body());
 		
 		//04 받은 Data Class화 
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		//04-01) JSON Type 유지
+//		JsonNode jsonNode = objectMapper.readTree(response.body());
+//		System.out.println("status : " + jsonNode.get("status"));
+//		System.out.println("total : " + jsonNode.get("total"));
+//		JsonNode data = jsonNode.get("data");
+//		for(int i=0;i<data.size();i++) {
+//			JsonNode item = data.get(i);
+//			System.out.println(item.get("BZ_NM"));
+//		}
+		
+		//04-02) JAVA Class 로 변환
+		
+		
 		
 	}
 
