1873081135c453cf4a4ae24eb074483094bf5fe7
jungwoogyun <jwg8910@naver.com>
2025-08-28 09:18:43 +0900

    13 DAY

---
+++ JAVA03/src/Ch06/C10SelenumAPIMain.java
@@ -0,0 +1,18 @@
+package Ch06;
+
+
+//크롬서버.jar 다운로드 경로(셀레니움 공식 홈페이지)
+//https://www.selenium.dev/downloads/
+
+//크롬드라이브 다운로드 경로
+//https://googlechromelabs.github.io/chrome-for-testing/#stable
+
+
+public class C10SelenumAPIMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
