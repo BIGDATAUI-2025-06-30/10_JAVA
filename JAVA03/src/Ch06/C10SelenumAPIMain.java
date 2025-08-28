f18ce1cea870b93cd01645ea9bde56c54ef7051c
jungwoogyun <jwg8910@naver.com>
2025-08-28 10:17:07 +0900

    13 DAY

--- JAVA03/src/Ch06/C10SelenumAPIMain.java
+++ JAVA03/src/Ch06/C10SelenumAPIMain.java
@@ -1,6 +1,13 @@
 package Ch06;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import java.util.UUID;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -19,7 +26,7 @@ public class C10SelenumAPIMain {
 	private static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
 	private static final String WEB_DRIVER_PATH = "src/Drivers/chromedriver.exe";
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		//Selenum 3.x 기준 브라우저 활성화 방법(4.x 부터는 chromedriver.exe 를 윈도우 PATH에 등록시 경로설정필요없음)
 		System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
 		
@@ -43,11 +50,67 @@ public class C10SelenumAPIMain {
 	
 		//쇼핑버튼 클릭 (.api_flicking_wrap .tab:nth-child(1) )
 		WebElement shoppingBtnEl = driver.findElement(By.cssSelector(".api_flicking_wrap .tab:nth-child(1)"));
+		//새창열기(target="_blank") 제거
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].removeAttribute('target')",shoppingBtnEl);		
+		
 		shoppingBtnEl.click();
 		
+		Thread.sleep(500);
 		
-		//창닫기
-		//driver.close();
+//		//네이버포털 -> '노트북'검색 -> 쇼핑 클릭-> 삼성 노트북(엔터) -> 리뷰 많은순 클릭
+		List<WebElement> Els =  driver.findElements(By.cssSelector(".subFilter_sort__lhuHl"));
+		Els.forEach(el->{
+			if(el.getText().contains("리뷰 많은순"))
+				el.click();
+		});
+//		
+//		//영역내 모든 정보 긁어오기 basicList_list_basis__uNBZx
+		List<WebElement> El2s =  driver.findElements(By.cssSelector(".basicList_list_basis__uNBZx"));
+		
+		
+		//파일로 저장
+		Writer out = new FileWriter("C:\\IOTEST\\"+UUID.randomUUID()+".html");
+	
+		
+        // HTML 기본 구조 작성
+        StringBuilder htmlContent = new StringBuilder();
+        htmlContent.append("<!DOCTYPE html>\n");
+        htmlContent.append("<html lang=\"ko\">\n");
+        htmlContent.append("<head>\n");
+        htmlContent.append("<meta charset=\"UTF-8\">\n");
+        htmlContent.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
+        htmlContent.append("<title>네이버 쇼핑 검색 결과</title>\n");
+        
+        
+		//css link 빼내오기
+		List<WebElement> cssLinks = driver.findElements(By.tagName("link"));
+		for(WebElement link : cssLinks) {
+			String rel = link.getAttribute("rel");
+			if(rel.equals("stylesheet")){
+				String href = link.getAttribute("href");
+				System.out.println("CSS 링크 : " + href);
+                htmlContent.append("<link rel=\"stylesheet\" href=\"").append(href).append("\">\n");
+
+			}
+		}
+        htmlContent.append("</head>\n");
+        htmlContent.append("<body>\n");
+        
+		//본문내용
+		for(WebElement el :El2s) {
+			String elHTML = el.getAttribute("outerHTML");
+			htmlContent.append(elHTML).append("\n");
+			
+		}
+        htmlContent.append("</body>\n");
+        htmlContent.append("</html>");
+        
+        // 파일에 저장
+        out.write(htmlContent.toString());
+        
+		out.flush();
+		out.close();
 
 	}
 
---
+++ JAVA03/src/Ch06/C11RestRequestResponseMain.java
@@ -0,0 +1,31 @@
+package Ch06;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class C11RestRequestResponseMain {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		//01 요청URL 파라미터 지정
+		String url = "https://www.daegufood.go.kr/kor/api/tasty.html";
+		String mode = "json";
+		String addr = "중구";
+		url = url + "?mode=" + mode + "&addr=" + addr;
+		
+		//02 Http Request 구성
+		HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+		
+		//03 Request 요청 -> Response 받기
+		HttpClient httpClient = HttpClient.newHttpClient();
+		HttpResponse<String> response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+		System.out.println(response.body());
+		
+		//04 받은 Data Class화 
+		
+	}
+
+}
