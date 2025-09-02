6a921063f4844e419920f2c60f00d6ba552b63a7
jungwoogyun <jwg8910@naver.com>
2025-09-02 09:48:46 +0900

    14 DAY

--- JAVA03/src/Ch09/C01Server.java
+++ JAVA03/src/Ch09/C01Server.java
@@ -15,19 +15,34 @@ public class C01Server {
 		ServerSocket server = new ServerSocket(7000);// IP:192.168.5.50 PORT : 7000
 		System.out.println("[INFO] SERVER SOCKET LISTENING");
 		
-		Socket client = server.accept();	// 1회
-		OutputStream out =  client.getOutputStream();
-		DataOutputStream dout = new DataOutputStream(out);
-		System.out.println("접속자 IP : " + client.getInetAddress());
-		dout.writeUTF("[SERVER] WELCOM TO SERVER + " + new Date());
-		dout.flush();
+		//1회접속
 		
-		dout.close();
-		out.close();
-		client.close();
-		server.close();
+//		Socket client = server.accept();	// 1회
+//		OutputStream out =  client.getOutputStream();
+//		DataOutputStream dout = new DataOutputStream(out);
+//		System.out.println("접속자 IP : " + client.getInetAddress());
+//		dout.writeUTF("[SERVER] WELCOM TO SERVER + " + new Date());
+//		dout.flush();
 		
-		System.out.println("[INFO] SERVER EXIT");
+		//server.close();
+		//System.out.println("[INFO] SERVER EXIT");
+		
+		//루프 접속
+		while(true)
+		{
+			Socket client = server.accept();
+			OutputStream out =  client.getOutputStream();
+			DataOutputStream dout = new DataOutputStream(out);
+			System.out.println("접속자 IP : " + client.getInetAddress());
+			dout.writeUTF("[SERVER] WELCOM TO SERVER + " + new Date());
+			dout.flush();
+			dout.close();
+			out.close();
+			client.close();
+		}	
+		
+
+
 		
 	}
 }
